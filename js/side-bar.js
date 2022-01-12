527f50be07040b8a63b2c24b2c3f4a55c9ba764f
valentin-diaz <valentin.diaz@uc.cl>
2022-01-12 15:09:52 -0300

    responsive mobile

--- js/side-bar.js
+++ js/side-bar.js
@@ -2,14 +2,13 @@ var bars_btn = document.getElementById('bars')
 var side_menu = document.getElementById('side-menu')
 var close_btn = document.getElementById('close')
 
-bars_btn.addEventListener('click', function(){
-    // side_menu.style.visibility = 'visible'
-    // side_menu.style.width = '100%'
-    side_menu.style.transform = 'translateX(0)'
-})
+function translateX(element, perc){
+    element.style.transform = `translateX(${perc}%)`
+}
 
+bars_btn.addEventListener('click', function(){
+    translateX(side_menu, 0)
+})
 close_btn.addEventListener('click', function(){
-    // side_menu.style.width = '0'
-    // side_menu.style.visibility = 'hidden'
-    side_menu.style.transform = 'translateX(100%)'
+    translateX(side_menu, 100)
 })
